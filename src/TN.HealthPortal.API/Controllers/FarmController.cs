b0f3ff8f3a9cd3fe32ee22e1ebc2fe878f97230e
Roel Stevens <RMWM.Stevens@student.han.nl>
2022-11-01 18:11:07 +0000

    Merged PR 5: Added API project with one dummy FarmController
    Added API project with one dummy FarmController
    
    Related work items: #35

---
+++ src/TN.HealthPortal.API/Controllers/FarmController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TN.HealthPortal.Lib.Models;
+
+namespace TN.HealthPortal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FarmController : Controller
+    {
+        [HttpGet]
+        [Route("{blnNumber}")]
+        public async Task<IActionResult> GetById(string blnNumber)
+        {
+            return Ok(new Farm()
+            {
+                Name = "Farm 1",
+                BlnNumber = blnNumber
+            });
+        }
+    }
+}
