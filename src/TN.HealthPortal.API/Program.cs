671ef694c1413868dff375fe07086db7e2d4021c
Roel Stevens <RMWM.Stevens@student.han.nl>
2022-12-08 18:43:48 +0000

    Merged PR 28: Removed IgnoreCycles
    Removed IgnoreCycles
    
    Related work items: #69

--- src/TN.HealthPortal.API/Program.cs
+++ src/TN.HealthPortal.API/Program.cs
@@ -19,8 +19,6 @@ builder.Services.AddDataLayer(builder.Configuration);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddControllers().AddJsonOptions(x =>
-                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
